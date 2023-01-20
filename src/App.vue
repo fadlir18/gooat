f25891847b7e495e41a25dae96b3d59e6ed74140
fadlir18 <rachmanfadlir@gmail.com>
2023-01-20 15:12:51 +0700

    Init + add auth controller

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,5 @@
 </template>
 
 <style lang="scss">
-  @import url(./css/caricari-style.css);
+  @import url(./css/808mansion-style.css);
 </style>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -7,7 +7,7 @@
 </template>
 
 <style lang="scss">
-  @import url(../css/caricari-style.css);
+  @import url(../css/808mansion-style.css);
   #AccountView{
     min-height: 70vh;
   }
